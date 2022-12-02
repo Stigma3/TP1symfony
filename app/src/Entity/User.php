e2793545d65444c94ba82c945d99937128eb4a6d
Stigma3 <Dorian.lucas87@gmail.com>
2022-12-02 15:23:45 +0100

    Api ok

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -12,15 +12,9 @@ use App\Repository\PostRepository;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 
-#[ApiResource(
-    collectionOperations: ['get' => ['normalization_context' => ['groups' => 'Post:list']], 'post' => ['normalization_context' => ['groups' => 'Post:list']]],
-    itemOperations: ['get' => ['normalization_context' => ['groups' => 'Post:item']], 'put' => ['normalization_context' => ['groups' => 'Post:item']], 'patch' => ['normalization_context' => ['groups' => 'Post:item']], 'delete' => ['normalization_context' => ['groups' => 'Post:item']]],
-    order: ['message' => 'DESC', 'id' => 'ASC'],
-    paginationEnabled: false,
-)]
+#[ApiResource]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')] 
-#[ApiRessource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
