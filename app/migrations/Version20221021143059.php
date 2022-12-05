1253912a086cf6d47e4dada3067e7ee576b2a34e
Stigma3 <Dorian.lucas87@gmail.com>
2022-12-05 10:39:41 +0100

    base a jour

--- app/migrations/Version20221021143059.php
+++ app/migrations/Version20221021143059.php
@@ -21,6 +21,10 @@ final class Version20221021143059 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(25) NOT NULL');
+
+        $this->addSql("INSERT INTO `user` (`id`, `email`, `roles`, `password`, `name`, `surname`, `pseudo`) VALUES
+        (1, 'test1@gmail.com', '[]', '\$2y\$13\$knbyvyLevuc6W22RHQquIORWjrMdl7r8wMxUevrOoKVCjaALgsmde', 'Test', 'test', 'leTest'),
+        (2, 'test2@gmail.com', '[\"utilisateur\"]', '\$2y\$13\$NjjYvkJw5rlq5D/7MccWZ.fRaC0dytr1zx8iV8gwxJ0yLIYx4MR5C', 'Joh', 'Nathan', 'AdileJoh');");
     }
 
     public function down(Schema $schema): void
